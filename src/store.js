c0f027b608bdb57e4d6ee0891ae5ce874b53732a
yassoelfar <yassermuhamed78@gmail.com>
2025-03-03 22:35:37 +0200

    remove duplicate result

--- src/store.js
+++ src/store.js
@@ -3,7 +3,12 @@ import { create } from 'zustand';
 const useStore = create((set) => ({
   translatedText: [],
   appendTranslatedText: (text) =>
-    set((state) => ({ translatedText: [...state.translatedText, text] })),
+    set((state) => {
+      if (state.translatedText.length > 0 && state.translatedText[state.translatedText.length - 1] === text) {
+        return state; 
+      }
+      return { translatedText: [...state.translatedText, text] };
+    }),
   resetTranslatedText: () => set({ translatedText: [] }),
 }));
 
